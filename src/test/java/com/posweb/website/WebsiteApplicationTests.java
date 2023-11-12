b490b8de938b2842911d2c4f1010d254b52a5dd5
VietHieppp <109458359+VietHieppp@users.noreply.github.com>
2023-11-12 11:16:13 +0700

    first commit javatechfinal

---
+++ src/test/java/com/posweb/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posweb.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
